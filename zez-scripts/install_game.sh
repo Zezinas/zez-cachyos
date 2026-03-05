380270d7741cd08a4a7e0e830c0f3847dc992569
Zezinas <zezinas@zezinas-macbook.local>
2026-03-05 13:03:19 +0200

    Started working on gaming script

---
+++ zez-scripts/install_game.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# sudo pacman -S steam
+
+# sudo pacman -S cachyos-gaming-meta
+
+# gamescope mangohud lib32-mangohud
+
+# CS2 / CS:GO
+# GTA V Enhanced (+mods)
+# Assetto Corsa (+mods)
